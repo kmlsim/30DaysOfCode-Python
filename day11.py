a57932debb21125c584c9ade134812b30b68b19b
kmlsim <kamilasim@yahoo.com.br>
2016-12-29 00:52:26 -0200

    Exercise working and all test cases passed.

--- day11.py
+++ day11.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 #!/bin/python
 
-# table= [ [ 0 for i in range(5) ] for j in range(5):  ]
-
-#[[row[i] for row in matrix] for i in range(4)]
-n = 2
+n = 6
 arr = []
+x = 0
+maxHourglass = 0
+greater = 0
+lista = []
 
 for arr_i in xrange(n):
    arr_temp = map(int,raw_input().strip().split(' '))
@@ -13,7 +14,15 @@ for arr_i in xrange(n):
 
 for i in range(n):
     for j in range(n):
-        if ((arr[i][j]) > 9):
-            print "Element A%d,%d it's out of range" % (i, j)
+        if ((arr[i][j]) >= -9 and (arr[i][j]) <= 9):
+            while ( x < n-2):
+                y = 0
+                while (y < n-2):
+                    sumHourglass = ((arr[0+x][0+y]) + (arr[0+x][1+y]) + (arr[0+x][2+y])  + (arr[1+x][1+y]) \
+                    + (arr[2+x][0+y]) + (arr[2+x][1+y]) + (arr[2+x][2+y]))
+                    y += 1
+                    lista.append(sumHourglass)
+                x += 1
         else:
-            for i in range(n):
+            print "Element A%d,%d it's out of range" % (i, j)
+print max(lista)
