f6f453c18911ca7ab91cc49b6961943957697793
kmlsim <kamilasim@yahoo.com.br>
2017-01-01 21:58:38 -0200

    Add day13 resolution.

---
+++ day13.py
@@ -0,0 +1,26 @@
+from abc import ABCMeta, abstractmethod
+class Book:
+    __metaclass__ = ABCMeta
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author
+    @abstractmethod
+    def display(): pass
+
+#Write MyBook class
+class MyBook(Book):
+    """docstring for ."""
+    def __init__(self, title, author,price):
+        super(MyBook, self).__init__(title, author)
+        self.price = price
+    def display(self):
+        print "Title:", title
+        print "Author:", author
+        print "Price:", price
+
+
+title=raw_input()
+author=raw_input()
+price=int(raw_input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
