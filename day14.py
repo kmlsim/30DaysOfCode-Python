3a1a7239c741813e23fcf7eaa1311d45c5e4709a
kmlsim <kamilasim@yahoo.com.br>
2017-01-02 20:14:33 -0200

    Starting solving problem day 14.

---
+++ day14.py
@@ -0,0 +1,16 @@
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+# Add your code here
+    def computeDifference(self):
+
+
+# End of Difference class
+
+_ = raw_input()
+a = [int(e) for e in raw_input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print d.maximumDifference
