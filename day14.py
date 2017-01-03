61f2e7948b51eccf50475f0c09c380797a23f553
kmlsim <kamilasim@yahoo.com.br>
2017-01-03 21:28:54 -0200

    Modify file. Still need correct the bug.

--- day14.py
+++ day14.py
@@ -1,34 +1,30 @@
-# import pdb; pdb.set_trace()
 class Difference:
-    listaDeValores = []
-    #maximumDifference = 0
     def __init__(self, a):
         self.__elements = a
         self._ = _
-        #self.maximumDifference = maximumDifference
-# Add your code here
+        self.maximumDifference = maximumDifference
+
     def computeDifference(self):
         global _
         global maximumDifference
-
+        listaDeValores = []
+        _ = int(_)
         if (_ >= 1 and _ <= 10):
-            for _ in range(0, 10):
-                if (a[n] in range(1, 100)):
+            for n in range(0, _):
+                if (a[n] >= 1 and a[n] <= 100):
                     sizeLista= len(a)
                     for i in range(sizeLista):
                         for y in range(sizeLista):
                             if (i != y):
                                 valor = abs((a[i]) - (a[y]))
                                 listaDeValores.append(valor)
-                    maximumDifference = max(listaDeValores)
+                    self.maximumDifference = max(listaDeValores)
 
-        # return maximumDifference
-
-
+# self.maximumDifference = maximumDifference
+# maximumDifference = 0
 _ = raw_input()
 a = [int(e) for e in raw_input().split(' ')]
 
 d = Difference(a)
 d.computeDifference()
-
 print d.maximumDifference
